3baebcf19939de0cd404c227f9619298e7e4ba36
DaniilGrigoritsa <104520210+DaniilGrigoritsa@users.noreply.github.com>
2022-09-18 20:50:59 +0300

    Update ERC1155.sol

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -1,38 +1,10 @@
 // SPDX-License-Identifier: ICS
 pragma solidity ^0.8.15;
 
+import "./IERC1155.sol";
 
-interface IERC1155 {
-    
-    event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _value);
-
-    event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _ids, uint256[] _values);
-
-    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-
-    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external;
-
-    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external;
-
-    function balanceOf(address _owner, uint256 _id) external view returns (uint256);
-
-    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory);
-
-    function setApprovalForAll(address owner, address _operator, bool _approved) external;
-
-    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
-}
-
-interface ERC1155TokenReceiver {
-    
-    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4);
-
-    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4);       
-
-}
 
 contract ERC1155 is IERC1155 {
-
 
     mapping(uint => mapping(address => uint)) private balances;
 
@@ -43,6 +15,7 @@ contract ERC1155 is IERC1155 {
         require((_owner) != address(0));
         return balances[_id][_owner];
     }
+    
 
     function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint[] memory){
         require(_owners.length == _ids.length);
@@ -76,6 +49,7 @@ contract ERC1155 is IERC1155 {
         emit TransferSingle(operator, from, to, id, amount);
     }
 
+
     function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external{
         require(_values.length == _ids.length);
 
@@ -84,11 +58,13 @@ contract ERC1155 is IERC1155 {
         }
     }
 
+
     function setApprovalForAll(address owner, address operator, bool approved) external {
         require(owner != operator);
         approvals[owner][operator] = approved;
         emit ApprovalForAll(owner, operator, approved);
     }
+
 
     function isApprovedForAll(address _owner, address _operator) public view returns (bool){
         return approvals[_owner][_operator];
